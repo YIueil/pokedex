8efd2f36a3aebb10ff37eab448089e872fcc41df
YIueil <YIueil@163.com>
2022-05-11 16:48:54 +0800

    基础框架内容搭建

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <PokeIndex></PokeIndex>
+</template>
+
+<script>
+import PokeIndex from './views/PokeIndex.vue';
+
+export default {
+  name: 'App',
+  components: {
+    PokeIndex,
+  },
+};
+</script>
+
+<style>
+/*全局样式配置*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 1160px;
+}
+</style>
---
+++ src/components/Layout/BasicLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    基础结构
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasicLayout',
+};
+</script>
+
+<style scoped>
+
+</style>
